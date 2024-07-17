a9ebe26df9d2ee4aa78c8706a5e095440a8912b0
luanlushera <149563902+luanlushera@users.noreply.github.com>
2024-07-17 13:26:53 +0700

    Initial commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+export default {
+  colors: {
+    black: "#000000",
+    white: "#ffffff",
+    primary: "#09090b",
+    secondary: "#F4CE14",
+    tertiary: "#EE9972",
+    quaternary: "#FBDABB",
+    highlight_light: "#EDEDEE",
+    highlight_dark: "#333333",
+  },
+  border: {
+    radius: "1.6rem",
+  },
+  font: {
+    family: {
+      primary: "'Noto Serif', serif",
+      secondary: "'Inter', sans-serif",
+    },
+    weight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+      extrabold: 800,
+    },
+    size: {
+      small: "1.6rem",
+      medium: "1.8rem",
+      large: "2.0rem",
+      xlarge: "4.0rem",
+      xxlarge: "5.2rem",
+      huge: "6.4rem",
+    },
+  },
+};
